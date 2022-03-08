f5e97c6c37b0c3b897810193d27488505de4dd4a
Vaibhav <vaibhav_srivastava_i@icloud.com>
2022-03-08 22:37:16 +0530

    Initial Commit

---
+++ Shared/CS1P3P2021_Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS1P3P2021_Day1App.swift
+//  Shared
+//
+//  Created by Vaibhav Srivastava on 08/03/22.
+//
+
+import SwiftUI
+
+@main
+struct CS1P3P2021_Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
