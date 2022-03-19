65c8ac038873a408f9d9b2ff8712e2d78d624f49
Vaibhav <vaibhav_srivastava_i@icloud.com>
2022-03-19 20:05:42 +0530

    Lecture 2 CSP1P3P2021

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,15 +7,71 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+struct ContentView : View{
+    @State var emojis = ["🤣","🤤","😀","😎","🐹","🦊","🤨","😜","😇"]
+    @State var emojiCount = 3
+    var body: some View{
+        VStack {
+            HStack{
+                ForEach(emojis[0..<emojiCount], id: \.self){emoji in
+                    CardView(content: emoji )
+                }
+                Spacer()
+                Group {
+                    VStack{
+                        add
+                        Spacer()
+                        remove
+                    }
+                } .padding(.vertical)
+                
+            }
+            .padding(.horizontal)
+            .foregroundColor(.red)
+        }
+    }
+    var add : some View {
+        Button(action: {emojiCount += 1 }, label: {
+            HStack{
+   //             Text("Add Card")
+                Image(systemName: "plus.circle")
+                
+            }
+        })
+    }
+    var remove : some View{
+        Button(action: {emojiCount -= 1}, label: {
+            HStack{
+              //  Text("Remove Card")
+                Image(systemName: "minus.circle")
+
+            }
+        })
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+
+
+struct CardView : View {
+    var content : String
+    @State var isfaceup = true
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isfaceup == false {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 4)
+                Text(content).font(.largeTitle)
+            } else{
+                shape.fill()
+            }
+        }
+        .onTapGesture{
+            isfaceup = !isfaceup
+        }
+        
     }
+    
 }
+
